c753b7a8bf4b41506cced1b6138677ab8be8d7f8
suuzanemoura <[suuzanemoura@gmail.com]>
2023-01-20 16:56:22 -0300

    Page estruturada e customizada + responsividade telas notebook e tablet

---
+++ scrollReveal.js
@@ -0,0 +1,26 @@
+function initScrollReveal() {
+    const scrollReveal = ScrollReveal({
+        origin: "top",
+        duration: 2000,
+        delay: 100,
+    })
+  
+    scrollReveal.reveal(".scroll-reveal", { distance: "3rem" })
+    scrollReveal.reveal(".scroll-reveal-small", { distance: "1rem" })
+    scrollReveal.reveal(".scroll-reveal-big", { distance: "4rem" })
+}
+
+
+function initScrollRevealLeft() {
+    const scrollRevealLeft = ScrollReveal({
+        origin: "left",
+        distance: "2rem",
+        duration: 1000,
+        delay: 100
+      })
+    
+    scrollRevealLeft.reveal(".scroll-reveal-left")
+}
+
+initScrollReveal()
+initScrollRevealLeft()
